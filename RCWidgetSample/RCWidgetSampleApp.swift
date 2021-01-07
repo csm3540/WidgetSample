f282a7194761510e719ab8665ca7ec4c4571ad4f
csm <csm@richplanet.kr>
2021-01-07 14:37:38 +0900

    Initial Commit

---
+++ RCWidgetSample/RCWidgetSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  RCWidgetSampleApp.swift
+//  RCWidgetSample
+//
+//  Created by 아이맥 on 2021/01/07.
+//
+
+import SwiftUI
+
+@main
+struct RCWidgetSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
